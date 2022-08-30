e7fb6fb59c61a36eba42a4610d53fa7712cb8520
alepadu <alepadualino@gmail.com>
2022-08-30 15:40:10 +0200

    3 files modified

--- exampleDynamo.js
+++ exampleDynamo.js
@@ -1,21 +1,47 @@
-import { CreateTableCommand } from "@aws-sdk/client-dynamodb";
-import { ddbClient } from "./dynamoDbClient";
+import { CreateTableCommand } from '@aws-sdk/client-dynamodb';
+import { ddbClient } from './dynamoDbClient';
 const params = {
-    AttributeDefinitons: [
-        {
-            AttributeName: "Season",
-            AttributeType: "N"
-        }, 
-        {
-            AttributeName: "Episode",
-            AttributeType:"N"
-        },
-        {
-            AttributeName: "Transmitted",
-            AttributeType: "BOOL"
-        }
-    ], 
-    KeySchema: [
-        
-    ]
-}
+  AttributeDefinitons: [
+    {
+      AttributeName: 'Season',
+      AttributeType: 'N',
+    },
+    {
+      AttributeName: 'Episode',
+      AttributeType: 'N',
+    },
+    {
+      AttributeName: 'Transmitted',
+      AttributeType: 'BOOL',
+    },
+  ],
+  KeySchema: [
+    {
+      AttributeName: 'Season',
+      KeyType: 'HASH',
+    },
+    {
+      AttributeName: 'Episode',
+      KeyType: 'RANGE',
+    },
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 1,
+    WriteCapacityUnits: 1,
+  },
+  TableName: 'EPISODE',
+  StreamSpecification: {
+    StreamEnabled: false,
+  },
+};
+
+export const run = async () => {
+  try {
+    const data = await ddbClient.send(new CreateTableCommand(params));
+    console.log('Table created', data);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+run();
